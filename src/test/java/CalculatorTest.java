1589c8cf9ef97c25295966e18d4cdd4dbb93a177
Egor_Grigorev <login7889@yandex.ru>
2025-10-07 18:21:46 +0300

    ver 0.0

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd(){
+
+    }
+
+
+
+    @Test
+    public void testSubtract(){
+
+    }
+
+
+
+}
